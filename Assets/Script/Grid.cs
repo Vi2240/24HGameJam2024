8118ba327e27ae5eafe036667c021d9b9cffcf36
Alex (Alexander) Nilsson <alexander.nilsson4@learnet.se>
2024-05-29 10:43:27 +0200

    Stage Test

---
+++ Assets/Script/Grid.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public int gridX;
+    public int gridY;
+
+    public GameObject gridPiece;
+
+}
