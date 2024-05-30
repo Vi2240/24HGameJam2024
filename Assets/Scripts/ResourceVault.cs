b3d94f2e34e93f9312a18bc68bbd0b7061507c7e
Vi2240 <vincentivarsson27@gmail.com>
2024-05-30 10:23:06 +0200

    Reuser+Hud+MinimapSomInteHeltFunkar+MainMeny

---
+++ Assets/Scripts/ResourceVault.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ResourceVault : MonoBehaviour
+{
+    [Header("ItemID \n Money = 0 \n Wood = 1 \n Stone = 2 \n Iron = 3")]
+    [SerializeField] float[] items;
+
+    [Header("Text \n Money = 0 \n Wood = 1 \n Stone = 2 \n Iron = 3")]
+    [SerializeField] TextMeshProUGUI[] text;
+
+
+    private void Start()
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            text[i].text = items[i].ToString();
+        }
+    }
+
+    public void ModifyItemByID(int id, float amount)
+    {
+        items[id] += amount;
+
+        text[id].text = items[id].ToString();
+    }
+}
